42aed8a4484d2d6dc7904b651a1e6a18b063b89f
ZephyrWn <4377445@gmail.com>
2026-06-16 21:51:29 +0800

    添加购物车业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -0,0 +1,43 @@
+package com.sky.controller.user;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+import com.sky.result.Result;
+import com.sky.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user/shoppingCart")
+@Slf4j
+public class ShoppingCartController {
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    /**
+     * 添加购物车
+     *
+     * @param shoppingCartDTO
+     * @return
+     */
+    @PostMapping("/add")
+    public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO) {
+        log.info("添加购物车：{}", shoppingCartDTO);
+        shoppingCartService.addShoppingCart(shoppingCartDTO);
+        return Result.success();
+    }
+
+    @GetMapping("/list")
+    public Result<List<ShoppingCart>> list() {
+        log.info("list shopping cart");
+        List<ShoppingCart> list = shoppingCartService.showShoppingCart();
+        return Result.success(list);
+    }
+}
